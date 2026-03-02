42ec6689b55c532731b648ddad0f1e29ff24bb3c
Jhr29j <juniorjoestudent@gmail.com>
2026-03-01 21:43:18 -0400

    Archivos nuevos

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 1200,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'electron', 'preload.js')
+    }
+  });
+
+  win.setMenu(null);
+
+  win.loadFile(path.join(__dirname, 'electron', 'render', 'login.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
